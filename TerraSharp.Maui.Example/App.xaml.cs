7389048cca64f203ca9ba93ed9efcf5abcb22605
wamal <wamal@SURFACEPRO4>
2023-02-22 10:42:13 +0100

    themes via app.xaml.cs

--- TerraSharp.Maui.Example/App.xaml.cs
+++ TerraSharp.Maui.Example/App.xaml.cs
@@ -5,6 +5,14 @@
         public App()
         {
             InitializeComponent();
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            if (mergedDictionaries != null)
+            {
+                mergedDictionaries.Clear();
+
+                mergedDictionaries.Add(new LightTheme());
+                mergedDictionaries.Add(new SharedStyles());
+            }
 
             MainPage = new AppShell();
         }
